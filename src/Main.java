2d3d0500115a3b99aadfe19a82a45851a3e467da
Костя <kroshavr@mail.ru>
2022-12-06 21:43:38 +0500

    На проверку

--- src/Main.java
+++ src/Main.java
@@ -15,6 +15,31 @@ public class Main {
             }
             }
             //Задание 2
-
+            int max = 90000;
+        for (int i = 0; i < arr.length; i++) {
+            if (arr[i] > max) {
+                max = arr [i];
+            }
+        }
+        int min = 210000;
+        for (int i = 0; i < arr.length; i++) {
+            if (arr[i] < min);
+            min = arr[i];
+        }
+        System.out.println("Минимальная сумма трат за день составила " + min + " рублей. Максимальная сумма трат за день составила " + max + " рублей.");
+        //Задание 3
+        double average = 0;
+        for (int i = 0; i < arr.length; i++) {
+            average = average + arr[i];
+            if (i == arr.length - 1) {
+                average = average / arr.length;
+                System.out.println("Средняя сумма трат за месяц составила " + average + " рублей.");
+            }
+        }
+        //Задание 4
+        char[] reverseFullName = { 'n', 'a', 'v', 'I', ' ', 'v', 'o', 'n', 'a', 'v', 'I'};
+        for (int i = reverseFullName.length - 1; i >= 0; i--) {
+            System.out.print(reverseFullName[i]);
+        }
         }
 }
